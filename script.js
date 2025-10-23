36f5e57e7810f893dd20ce7c330c9a99f07e152d
Jujo <28857218+jujogoar@users.noreply.github.com>
2025-10-23 11:18:35 +0200

    Game logic

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ function getComputerChoice() {
 function getHumanChoice() {
     return choices[prompt("Please enter your choice between 1 and 3 (1: rock, 2: paper, 3: scissors)")]
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    console.log(`HUMAN: ${humanChoice} VS COMPUTER: ${computerChoice}`)
+
+    if (humanChoice == computerChoice) {
+        console.log("DRAW!")
+        return
+    }
+
+    if ((humanChoice == "rock" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "rock")
+    ) {
+        console.log("COMPUTER WINS ROUND!")
+        computerScore++
+        return
+    }
+
+    if ((computerChoice == "rock" && humanChoice == "paper") ||
+        (computerChoice == "paper" && humanChoice == "scissors") ||
+        (computerChoice == "scissors" && humanChoice == "rock")
+    ) {
+        console.log("HUMAN WINS ROUND!")
+        humanScore++
+        return
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
