28f80fbca9f1b8b149f3a556a879fb49fcdcffb6
Jujo <28857218+jujogoar@users.noreply.github.com>
2025-10-23 11:04:30 +0200

    Function to ask the human of a choice

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const choices = ["rock", "paper", "scissors"]
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
+
+function getHumanChoice() {
+    return choices[prompt("Please enter your choice between 1 and 3 (1: rock, 2: paper, 3: scissors)")]
+}
